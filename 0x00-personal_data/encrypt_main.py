665c64b86b96a2a77a27c7d045da3ca479fde75d
brayohmurithidev <murithibrianm@gmail.com>
2023-05-02 18:08:15 +0300

    initial commit

--- 0x00-personal_data/encrypt_main.py
+++ 0x00-personal_data/encrypt_main.py
@@ -5,6 +5,13 @@ Main file
 
 hash_password = __import__('encrypt_password').hash_password
 
+hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
+
 password = "MyAmazingPassw0rd"
-print(hash_password(password))
-print(hash_password(password))
+encrypted_password = hash_password(password)
+print(encrypted_password)
+
+# password = "MyAmazingPassw0rd"
+# print(hash_password(password))
+# print(hash_password(password))
