2c66986238700d5b7da8fb0d4c86270ed7dc971b
brayohmurithidev <murithibrianm@gmail.com>
2023-05-02 20:46:39 +0300

    unauthorized 401

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'])
+def unauth() -> str:
+    """ GET /api/v1/status
+    Return:
+      - The unauthorized
+    """
+    abort(401)
