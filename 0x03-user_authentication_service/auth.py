96e188a33d0ae889a92a8a9300af13a30f8b73d7
brayohmurithidev <murithibrianm@gmail.com>
2023-05-12 00:07:07 +0300

    credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,9 @@
 AUTH MODULE
 '''
 import bcrypt
-import base64
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,10 +13,6 @@ def _hash_password(password: str) -> bytes:
     password = password.encode('utf-8')
     hashed = bcrypt.hashpw(password, bcrypt.gensalt())
     return hashed
-
-
-from db import DB
-from user import User
 
 
 class Auth:
@@ -25,12 +23,24 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        '''Register new user. 
+        '''Register new user.
         Use the methods from db class
         '''
-        user = self._db._session.query(User).filter(User.email == email).first()
-        if user is not None:
+        try:
+            user = self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
-        pwd = _hash_password(password)
-        newUser = self._db.add_user(email, pwd)
-        return newUser
+        except NoResultFound:
+            pwd = _hash_password(password)
+            newUser = self._db.add_user(email, pwd)
+            return newUser
+
+    def valid_login(self, email: str, password: str) -> bool:
+        '''Validit password'''
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
+                return True
+        except NoResultFound:
+            return False
+        else:
+            return False
